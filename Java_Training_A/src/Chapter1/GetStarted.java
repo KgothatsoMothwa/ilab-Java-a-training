012ff95fbf30f5c36840c0badb7daff4ce90fbbc
Kgothatso Mothwa <khomeiniblue07@gmail.com>
2025-01-06 11:15:28 +0200

    Program created using different data types #1

---
+++ Java_Training_A/src/Chapter1/GetStarted.java
@@ -0,0 +1,32 @@
+package Chapter1;
+
+public class GetStarted {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        //Declare
+        int age;
+        double height;
+        float salary;
+        boolean IsContractor;
+        String name;
+        char positionCode; //(S-Senior, J-Junior, I-Intern)
+
+        //Assign
+        age = 50;
+        height = 1.06;
+        salary = 1400.78F;
+        IsContractor =true;
+        name = "Blue";
+        positionCode = 'I';
+
+        //Use
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Salary: " + salary);
+        System.out.println("Position Code: " + positionCode);
+
+
+    }
+}
